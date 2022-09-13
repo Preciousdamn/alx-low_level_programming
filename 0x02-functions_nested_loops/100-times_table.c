a23d29348d7277607835ec11bfab456a79f59032
Precious <preciousikiki@gmail.com>
2022-09-13 16:50:45 -0700

    The World

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,50 @@
-#include "main.h"
+include "main.h"
 /**
- * print_to_98 - function
- *
- * @n: parameter
- *
- * Return: natural numbers
+ *  print_times_table - print multiplication table up to n
+ *  @n: integer argument
  */
-void print_to_98(int n)
+void print_times_table(int n)
 {
-	int upper = 98;
+	int row;
+	int column;
+	int product;
 
-	if (n > upper)
+	if (n >= 0 && n <= 15)
 	{
-		while (n >= upper)
+		for (row = 0; row <= n; row++)
 		{
-			if (n != upper)
+			for (column = 0; column <= n; column++)
 			{
-				printf("%d%s", n, ", ");
+				product = (row * column);
+				if (column == 0)
+
+					_putchar('0' + product);
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (product <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + product);
+					}
+					else if (product > 9 && product < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (product / 10));
+						_putchar('0' + (product % 10));
+					}
+					else if (product >= 100)
+					{
+						_putchar('0' + (product / 100));
+						_putchar('0' + ((product / 10) % 10));
+						_putchar('0' + (product % 10));
+					}
+				}
 			}
-			else
-			{
-				printf("%d", n);
-			}
-			n--;
+			_putchar('\n');
 		}
 	}
-	else
-	{
-		while (n <= upper)
-		{
-			if (n != upper)
-			{
-				printf("%d%s", n, ", ");
-			}
-			else
-			{
-				printf("%d", n);
-			}
-			n++;
-		}
-	}
-	putchar(10);
 }
 
