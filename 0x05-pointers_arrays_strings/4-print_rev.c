08fbe8b0bd7fc8460846d569c841a8ced28a1303
Precious <preciousikiki@gmail.com>
2022-09-19 08:40:31 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse order
+ * @s: pointer of parameter
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int length = strlen(s);
+
+	while (len--)
+		_putchar(*(s + len));
+	_putchar(10);
+}
