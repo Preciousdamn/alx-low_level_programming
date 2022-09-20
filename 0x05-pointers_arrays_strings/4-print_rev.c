b0ec3d301ff178c03c5b4fc8ea4e01ea91624930
Precious <preciousikiki@gmail.com>
2022-09-20 05:32:23 -0700

    4. I can only go one way

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
 /**
- * print_rev - prints a string in reverse order
+ * print_rev - function
+ *
  * @s: pointer of parameter
- * Return: Nothing
  */
-
 void print_rev(char *s)
 {
-	int length = strlen(s);
+	int length = 0;
 
-	while (len--)
-		_putchar(*(s + len));
+	while (s[length] != '\0')
+		length++;
+	while (length > 0)
+		_putchar(s[--length]);
 	_putchar(10);
 }
