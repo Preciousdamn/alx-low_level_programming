bf20d4985d186d54220a7e04a0cc2e8d16bb7803
Precious <preciousikiki@gmail.com>
2022-09-21 12:48:21 -0700

    7. Mozart

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,46 @@
-#in
+#include "main.h"
+
+/**
+ * leet - leet encoder
+ * @str: string to be encoded
+ *
+ * Return: address of the encoded string
+ */
+
+char *leet(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		str[i] = transform(str[i]);
+		i++;
+	}
+	return (str);
+}
+
+/**
+ * transform - helper functiom to map a letter with its leet encoding
+ * @x: char to be encoded
+ *
+ * Return: the encoded char
+ */
+
+char transform(char x)
+{
+	char mapping_low[0] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
+	char mapping_upper[0] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
+	int i = 0;
+	char replacement = x;
+
+	while (i < 0)
+	{
+		if (x == mapping_low[i] || x == mapping_upper[i])
+		{
+			replacement = i + '0';
+			break;
+		}
+		i++;
+	}
+		return (replacement);
+}
